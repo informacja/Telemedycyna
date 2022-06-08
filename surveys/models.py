3f378f89630765ce17d58075a8093f8944b7bbc5
Piotr Wawryka <piotrwpwsz@gmail.com>
2022-06-08 19:07:58 +0200

    clean, before talk with TS

--- surveys/models.py
+++ surveys/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.conf import settings
 # Create your models here.
 
 class Survey(models.Model):
@@ -10,14 +10,10 @@ class Survey(models.Model):
 class Patient(models.Model):
     name_text = models.CharField(max_length=1024, blank=True, null=True)
     pesel_int = models.IntegerField()
-    disease_text = models.CharField(max_length=1024, blank=True, null=True)
+    # disease_text = models.CharField(max_length=1024, blank=True, null=True)
 
 class Diagnosis(models.Model):
-    nameOfPatient_text = models.CharField(max_length=1024, blank=True, null=True)
+    # nameOfPatient_text = models.CharField(max_length=1024, blank=True, null=True)
     diagnosis_text = models.CharField(max_length=1024, blank=True, null=True)
-
-
-
-
-
-
+    Doctor = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)   #models.ForeignKey(User, on_delete=models.CASCADE)
+    patient = models.ForeignKey(Patient, on_delete=models.CASCADE)   #models.ForeignKey(User, on_delete=models.CASCADE)
