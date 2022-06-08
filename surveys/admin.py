729b99847f292ab428b0fe37836cce802c4e48ed
Piotr Wawryka <piotrwpwsz@gmail.com>
2022-06-08 13:59:31 +0200

    Admin

--- surveys/admin.py
+++ surveys/admin.py
@@ -1,11 +1,13 @@
 from django.contrib import admin
 
-from surveys.models import Survey
+from surveys.models import Survey, Patient,Diagnosis
 
 # Register your models here.
-
-
 
 @admin.register(Survey)
 class SurveytAdmin(admin.ModelAdmin):
     pass
+
+
+admin.site.register(Patient)
+admin.site.register(Diagnosis)
