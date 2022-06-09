0473a46e91846abbe066ba04eec991da2777b7cf
Angelika Przeliorz <artgelika@gmail.com>
2022-06-09 11:45:22 +0200

    Add export_csv information in urls.py and survey_filled.html

--- surveys/urls.py
+++ surveys/urls.py
@@ -2,6 +2,8 @@ from django.urls import path, include
 
 from surveys.views import *
 from . import views
+from .views import export_csv
+
 
 urlpatterns = [
     path('', views.home),
@@ -10,4 +12,8 @@ urlpatterns = [
     path(r'survey_stats/', SurveyStatsView.as_view(), name='survey_stats'),
     path(r'wav_details/<int:wav_pk>', WavFileDetailsView.as_view(), name='wav_details'),
     path(r'wav_list', WavlistView.as_view(), name='wav_list'),
+    path('export', export_csv, name='export_waves').
 ]
+
+
+
